c2bb66970620b2cee21014d7296db592bf7bddc2
James Trew <j.trew10@gmail.com>
2023-03-21 01:55:46 -0400

    more minesweeper logic

--- src/minesweeper.rs
+++ src/minesweeper.rs
@@ -15,7 +15,7 @@ pub enum GameState {
     Lose,
 }
 
-#[derive(Eq, PartialEq, Hash)]
+#[derive(Debug, Eq, PartialEq, Hash)]
 pub struct Pos {
     pub row: usize,
     pub col: usize,
@@ -44,6 +44,18 @@ impl Minesweeper {
         }
     }
 
+    pub fn from_vec(vec: Vec<Vec<usize>>) -> Self {
+        let mut game = Self::new(vec.len(), vec[0].len(), 0);
+        for i in 0..game.height {
+            for j in 0..game.width {
+                if vec[i][j] != 0 {
+                    game.board[i][j] = Cell::Mine;
+                }
+            }
+        }
+        game
+    }
+
     pub fn rand_mine_pos(&self) -> Pos {
         let mut tr = rand::thread_rng();
         Pos {
@@ -64,7 +76,7 @@ impl Minesweeper {
         }
     }
 
-    pub fn action_cell(&mut self, pos: Pos) {
+    pub fn lclick_cell(&mut self, pos: Pos) {
         match self.board[pos.row][pos.col] {
             Cell::Mine => self.state = GameState::Lose,
             Cell::Closed => self.open_cell(pos),
@@ -73,10 +85,20 @@ impl Minesweeper {
     }
 
     fn open_cell(&mut self, pos: Pos) {
-        self.iter_neighbors(pos).for_each(|_| todo!())
+        let neighboring_mines = self
+            .iter_neighbors(&pos)
+            .filter(|cell| self.board[cell.row][cell.col] == Cell::Mine)
+            .count();
+        self.board[pos.row][pos.col] = Cell::Open(neighboring_mines);
+        self.open_empty_neighbors(&pos);
     }
 
-    fn iter_neighbors(&self, pos: Pos) -> impl Iterator<Item = Pos> {
+    fn open_empty_neighbors(&mut self, _pos: &Pos) {
+        // self.iter_neighbors(&pos)
+        //     .filter(|cell| self.board[cell.row][cell.col] == Cell::Closed);
+    }
+
+    fn iter_neighbors(&self, pos: &Pos) -> impl Iterator<Item = Pos> {
         let row_start = if pos.row > 0 { pos.row - 1 } else { pos.row };
         let row_end = if pos.row < self.height - 1 {
             pos.row + 1
@@ -90,12 +112,14 @@ impl Minesweeper {
             pos.col
         };
 
-        (row_start..=row_end).flat_map(move |r| {
-            (col_start..=col_end).filter_map(move |c| {
-                if r == pos.row && c == pos.col {
+        let cell_row = pos.row;
+        let cell_col = pos.col;
+        (row_start..=row_end).flat_map(move |row| {
+            (col_start..=col_end).filter_map(move |col| {
+                if row == cell_row && col == cell_col {
                     None
                 } else {
-                    Some(Pos { row: r, col: c })
+                    Some(Pos { row, col })
                 }
             })
         })
@@ -105,6 +129,17 @@ impl Minesweeper {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::collections::HashSet;
+
+    fn assert_board(actual: Vec<Vec<Cell>>, expect: Vec<Vec<Cell>>) {
+        assert_eq!(expect.len(), actual.len());
+        for i in 0..expect.len() {
+            assert_eq!(expect[i].len(), actual[i].len());
+            for j in 0..expect[i].len() {
+                assert_eq!(expect[i][j], actual[i][j]);
+            }
+        }
+    }
 
     #[test]
     fn basic_board_init() {
@@ -115,17 +150,22 @@ mod tests {
             vec![Cell::Closed, Cell::Closed, Cell::Closed],
         ];
         let result = Minesweeper::new(4, 3, 0);
-        assert_eq!(expect.len(), result.board.len());
-        for i in 0..expect.len() {
-            assert_eq!(expect[i].len(), result.board[i].len());
-            for j in 0..expect[i].len() {
-                assert_eq!(expect[i][j], result.board[i][j]);
-            }
-        }
+        assert_board(result.board, expect);
         assert_eq!(result.mine_count, 0);
         assert_eq!(result.height, 4);
         assert_eq!(result.width, 3);
         assert_eq!(result.state, GameState::Playing);
+    }
+
+    #[test]
+    fn board_init_from_vec() {
+        let board = vec![vec![0, 1, 0, 1], vec![1, 0, 1, 0]];
+        let expect = vec![
+            vec![Cell::Closed, Cell::Mine, Cell::Closed, Cell::Mine],
+            vec![Cell::Mine, Cell::Closed, Cell::Mine, Cell::Closed],
+        ];
+        let game = Minesweeper::from_vec(board);
+        assert_board(game.board, expect);
     }
 
     #[test]
@@ -139,5 +179,66 @@ mod tests {
             .filter(|&cell| *cell == Cell::Mine)
             .count();
         assert_eq!(mine_count, game.mine_count);
+    }
+
+    #[test]
+    fn lclick_cell_with_mine() {
+        let mut game = Minesweeper::new(4, 3, 3);
+        game.board[0][0] = Cell::Mine;
+        assert_eq!(game.state, GameState::Playing);
+        game.lclick_cell(Pos { row: 0, col: 0 });
+        assert_eq!(game.state, GameState::Lose);
+    }
+
+    #[test]
+    fn lclick_cell_surrounded_by_mines() {
+        let mut game = Minesweeper::from_vec(vec![vec![0, 1], vec![1, 1]]);
+        assert_eq!(game.state, GameState::Playing);
+        game.lclick_cell(Pos { row: 0, col: 0 });
+        assert_eq!(game.state, GameState::Playing);
+        assert_eq!(game.board[0][0], Cell::Open(3));
+    }
+
+    #[test]
+    fn board_corner_iter_neighbors() {
+        let game = Minesweeper::new(4, 3, 3);
+        let ret: HashSet<_> = game.iter_neighbors(&Pos { row: 0, col: 0 }).collect();
+        let expect = HashSet::from([
+            Pos { row: 0, col: 1 },
+            Pos { row: 1, col: 0 },
+            Pos { row: 1, col: 1 },
+        ]);
+        assert_eq!(ret, expect);
+    }
+
+    #[test]
+    fn board_edge_iter_neighbors() {
+        let game = Minesweeper::new(4, 3, 3);
+        let ret: HashSet<_> = game.iter_neighbors(&Pos { row: 1, col: 0 }).collect();
+        let expect = HashSet::from([
+            Pos { row: 0, col: 0 },
+            Pos { row: 0, col: 1 },
+            Pos { row: 1, col: 1 },
+            Pos { row: 2, col: 0 },
+            Pos { row: 2, col: 1 },
+        ]);
+        assert_eq!(ret, expect);
+    }
+
+    #[test]
+    fn board_center_iter_neighbors() {
+        let game = Minesweeper::new(4, 3, 3);
+        let ret: HashSet<_> = game.iter_neighbors(&Pos { row: 1, col: 1 }).collect();
+        let expect = HashSet::from([
+            Pos { row: 0, col: 0 },
+            Pos { row: 0, col: 1 },
+            Pos { row: 0, col: 2 },
+            Pos { row: 1, col: 0 },
+            Pos { row: 1, col: 2 },
+            Pos { row: 2, col: 0 },
+            Pos { row: 2, col: 1 },
+            Pos { row: 2, col: 2 },
+        ]);
+        assert_eq!(ret, expect);
     }
 }
