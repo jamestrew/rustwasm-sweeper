ac2d9d98bcd4dc9249fb1b36b08f50be65e24ccb
James Trew <j.trew10@gmail.com>
2023-04-02 21:59:35 -0400

    write some board errors

--- src/minesweeper/board.rs
+++ src/minesweeper/board.rs
@@ -1,36 +1,26 @@
 use super::cell::Cell;
 use std::{error::Error, fmt::Display};
 
-#[derive(Debug, Eq, PartialEq, Hash)]
+#[derive(Debug, Eq, PartialEq, Hash, Clone, Copy)]
 pub struct Pos {
     pub row: usize,
     pub col: usize,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum BoardError {
-    SetCellError(String),
+    SetCellError { pos: Pos, cell: Cell },
 }
 
-impl Error for BoardError {
-    fn source(&self) -> Option<&(dyn Error + 'static)> {
-        None
-    }
-
-    fn description(&self) -> &str {
-        match self {
-            BoardError::SetCellError(_) => todo!(),
-        }
-    }
-
-    fn cause(&self) -> Option<&dyn Error> {
-        self.source()
-    }
-}
+impl Error for BoardError {}
 
 impl Display for BoardError {
-    fn fmt(&self, _f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            BoardError::SetCellError { pos, cell } => {
+                write!(f, "failed to set {:?} at {:?}", cell, pos)
+            }
+        }
     }
 }
 
@@ -44,16 +34,15 @@ impl Board {
         Board(board)
     }
 
-    pub fn get(&self, pos: &Pos) -> Option<&Cell> {
+    pub fn get(&self, pos: Pos) -> Option<&Cell> {
         match self.0.get(pos.row) {
             Some(row) => row.get(pos.col),
             None => None,
         }
     }
 
-    pub fn set(&mut self, pos: &Pos, cell: Cell) -> Result<(), BoardError> {
-        let err_msg = format!("Failed to set {:?} at {:?}", cell, pos);
-        let err = BoardError::SetCellError(err_msg);
+    pub fn set(&mut self, pos: Pos, cell: Cell) -> Result<(), BoardError> {
+        let err = BoardError::SetCellError { pos, cell };
         match self.0.get_mut(pos.row) {
             Some(row) => {
                 let c = row.get_mut(pos.col).ok_or(err)?;
@@ -90,5 +79,42 @@ mod tests {
             vec![Cell::Closed, Cell::Closed, Cell::Closed],
         ];
         assert_board(b, expect);
+    }
+
+    #[test]
+    fn board_get_inbounds() {
+        let mut b = Board::new(4, 3);
+        b.0[0][1] = Cell::Mine;
+        assert_eq!(b.get(Pos { row: 0, col: 1 }).unwrap(), &Cell::Mine);
+    }
+
+    #[test]
+    fn board_get_outbounds() {
+        let b = Board::new(4, 3);
+        assert_eq!(b.get(Pos { row: 10, col: 1 }), None);
+    }
+
+    #[test]
+    fn board_set_inbounds() {
+        let mut b = Board::new(4, 3);
+        let pos = Pos { row: 0, col: 0 };
+        assert_eq!(b.get(pos).unwrap(), &Cell::Closed);
+        assert_eq!(b.set(pos, Cell::Mine).unwrap(), ());
+        assert_eq!(b.get(pos).unwrap(), &Cell::Mine);
+    }
+
+    #[test]
+    fn board_set_outbounds() {
+        let mut b = Board::new(4, 3);
+        let pos = Pos { row: 100, col: 0 };
+        let res = b.set(pos, Cell::Mine);
+        assert!(res.is_err());
+        assert_eq!(
+            res.unwrap_err(),
+            BoardError::SetCellError {
+                pos,
+                cell: Cell::Mine
+            }
+        );
     }
 }
